7fb082d200d99549a9088ad9b08c6f6bd1da9dc9
aslahea <aslahea068@gmail.com>
2026-06-03 06:10:18 +0530

    Refactor: Move versions.tf directly to dev environment and remove root versions.tf

--- environments/dev/versions.tf
+++ environments/dev/versions.tf
@@ -1,1 +1,21 @@
-../../versions.tf
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "~> 3.0"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+  }
+}
